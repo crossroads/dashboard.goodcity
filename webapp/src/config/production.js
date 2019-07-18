ecbfb3c07c8945300abad957214b12bfe668ae31
Patrick R <patrixr@Patricks-MacBook-Pro.local>
2019-07-18 16:12:18 +0800

    use secure websockets

--- webapp/src/config/production.js
+++ webapp/src/config/production.js
@@ -2,6 +2,6 @@ export default {
   // Add configuration overrides
   endpoints: {
     http: 'https://gql.goodcity.hk/v1/graphql',
-    ws: 'ws://gql.goodcity.hk:8080/v1/graphql',
+    ws: 'wss://gql.goodcity.hk:8080/v1/graphql',
   }
 }
